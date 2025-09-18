4612461ac4406d06621e98fd0d489dc7fbbb9471
Yoma Ozoh <ogeneyoma.ozoh@stu.ocsb.ca>
2025-09-18 18:24:39 +0000

    Area and Perimeter of a rectangle

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Yoma Ozoh
+# Date: September 18, 2025
+# This program calculates the area and perimeter of a rectangle
+
+
+def main():
+    print("For a rectangle with length 76cm and width 52cm:")
+    print()
+    print("The area is: {}cm^2".format(76 * 52))
+    print("The perimeter is: {}cm".format(2 * (76 + 52)))
+
+
+if __name__ == "__main__":
+    main()
